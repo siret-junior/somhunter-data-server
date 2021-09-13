1a2d2bb93c5b5906cbabc1d0b67acea701c7ed39
Frank Mejzlik <frankmejzlik@gmail.com>
2021-09-13 17:28:00 +0200

    Fix path.

--- estatic.js
+++ estatic.js
@@ -8,7 +8,7 @@ const config_core = require("../somhunter-core/config/config-core.json");
 const data_dir = config_core.core.datasets.data_dir;
 const data_server_port = config_core.data_server.API.port;
 
-const dir = path.join(__dirname, "../somhunter-core/", data_dir);
+const dir = path.join(__dirname, "../somhunter-core/", data_dir, "media");
 
 console.log("(!!!)\nRunning at directory: ", __dirname, "\n(!!!)");
 console.log("Serving the directory: ", dir);
