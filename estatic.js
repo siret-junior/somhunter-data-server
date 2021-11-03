dd2d5d8fad6322b921d5e640ef0451b8e12ca743
Patrik Vesely <prtrikvesely@gmail.com>
2021-11-03 22:56:39 +0100

    Fixed path in config in UI

--- estatic.js
+++ estatic.js
@@ -4,13 +4,13 @@ const express = require('express');
 const app = express();
 
 const config_core = require("../somhunter-core/config/config-core.json");
-const dir = path.join(__dirname, "../somhunter-core/", config_core.core.data_dir);
+const dir = path.join(__dirname, "../somhunter-core/", config_core.core.datasets.data_dir);
 
 console.log("(!!!)\nRunning at directory: ", __dirname, "\n(!!!)");
 console.log("Serving the directory: ", dir);
 app.use(express.static(dir));
 
-app.listen(8889, function () {
-    console.log('Serving files on http://localhost:8889/');
+app.listen(8081, function () {
+    console.log('Serving files on http://localhost:8081/');
 });
 
