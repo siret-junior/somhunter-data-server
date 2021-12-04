d51248459a01178f8c7c9a30d803343de187c4c5
Frank Mejzlik <frankmejzlik@gmail.com>
2021-12-04 20:10:33 +0100

    Fix docker build.

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ printf "\tInstalling 'somhunter-data-server'...\n"
 ABSOLUTE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $ABSOLUTE_PATH # CD to script dir
 
-npm install
+npm install  > /dev/null
 
 printf "\tDone installing 'somhunter-data-server'.\n"
