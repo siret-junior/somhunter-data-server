92c8ff4edeb7a1ad80dfbf6f8a742cbc4b8855a8
Frank Mejzlik <frankmejzlik@gmail.com>
2022-01-26 11:07:51 +0100

    Add licensing.

--- estatic.js
+++ estatic.js
@@ -1,3 +1,22 @@
+/* This file is part of SOMHunter Data Server.
+ *
+ * Copyright (C) 2022    Frantisek Mejzlik <frankmejzlik@gmail.com>
+ *                       Vit Skrhak <vitek.skrhak@seznam.cz>
+ *                       Patrik Veselý <prtrikvesely@gmail.com>
+ * 
+ *  SOMHunter Data Server is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 2 of the License, or (at your option)
+ *  any later version.
+ * 
+ *  SOMHunter Data Server is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ * 
+ *  You should have received a copy of the GNU General Public License along with
+ *  SOMHunter Data Server. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 const path = require('path');
 const express = require('express');
